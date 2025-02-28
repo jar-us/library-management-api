ae428b624266201972d3492375902f698aaafd75
jar-us <suraj.sharma3963@gmail.com>
2025-02-28 11:34:09 +0530

    Add initial project setup for Spring Boot with Kotlin
    This commit sets up a basic Spring Boot project using Kotlin. It includes essential files like `pom.xml`, `application.properties`, and `LibraryManagementSystemApiApplication.kt`. Additionally, configuration files like `.gitignore`, `.gitattributes`, and Maven wrapper scripts are added for development convenience.

---
+++ src/main/kotlin/jar/us/librarymanagementsystemapi/LibraryManagementSystemApiApplication.kt
@@ -0,0 +1,11 @@
+package jar.us.librarymanagementsystemapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibraryManagementSystemApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<LibraryManagementSystemApiApplication>(*args)
+}
---
+++ src/test/kotlin/jar/us/librarymanagementsystemapi/LibraryManagementSystemApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package jar.us.librarymanagementsystemapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryManagementSystemApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
